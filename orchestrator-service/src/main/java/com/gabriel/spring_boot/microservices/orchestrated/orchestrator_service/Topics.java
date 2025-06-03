01930c2c96430b1f4194f767f36ea40835ac3b93
Gabriel M. Fernandes <gabriel.f@compound.direct>
2025-06-03 22:33:04 +0800

    feat: added kafka topics for each service

---
+++ orchestrator-service/src/main/java/com/gabriel/spring_boot/microservices/orchestrated/orchestrator_service/Topics.java
@@ -0,0 +1,22 @@
+package com.gabriel.spring_boot.microservices.orchestrated.orchestrator_service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Topics {
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAILURE("finish-failure"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAILURE("product-validation-failure"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAILURE("payment-failure"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAILURE("inventory-failure"),
+    NOTIFY_ENDING("notify-ending");
+
+    private String topic;
+}
