d7595c43009ff375e3cf7db57b634572473dda0d
Gabriel M. Fernandes <gabriel.f@compound.direct>
2025-06-04 19:00:55 +0800

    refactor: removed underscores from packages names

---
+++ inventory-service/src/main/java/com/gabriel/springboot/microservices/orchestrated/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.springboot.microservices.orchestrated.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
---
+++ order-service/src/test/java/com/gabriel/springboot/microservices/orchestrated/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.springboot.microservices.orchestrated.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
