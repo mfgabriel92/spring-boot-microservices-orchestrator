f07258fda1c2a27d0cba5ca18c790fd6b2b5c609
Gabriel M. Fernandes <gabriel.f@compound.direct>
2025-06-07 23:18:32 +0800

    feat: initial products data

---
+++ product-validation-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (id, code) VALUES ('b7a42720-dbda-45e1-b2fb-eeda3816d58d', 'COMIC_BOOKS');
+INSERT INTO products (id, code) VALUES ('33eab678-5582-4fdf-b909-0363aece2acb', 'BOOKS');
+INSERT INTO products (id, code) VALUES ('183f5852-ee19-44b0-b77a-9291c1f631cf', 'MOVIES');
+INSERT INTO products (id, code) VALUES ('255a57d6-c755-44e1-9fa3-ec02625cf377', 'MUSIC');
